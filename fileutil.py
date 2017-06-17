8ccbbfc70eb37b7d5839e30b67e5f264e28232f8
ctrmint <mark@mrodman.com>
2017-06-17 23:45:46 +0100

    Start

--- fileutil.py
+++ fileutil.py
@@ -14,14 +14,25 @@ class myfile(object):
         print "get size"
 
 
-def getfilesfromdir(directory):
-    print directory
-
-    return
+def getfilesfromdir(directory, depth):
+    # directory - the starting directory to be scanned for files
+    # depth is the level to dig, 0 is cwd, 1 is first subdirectory
+    file_paths = []
+    i = 0
+    depth
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+        i += 1
+        if i > depth:
+            break
+    return (file_paths)
 
 
 def main():
-    getfilesfromdir()
+    getfilesfromdir("/Users/mark/PycharmProjects/fileutils", 0)
     return
 
 

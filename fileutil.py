de3f416570c8c289674d082aad4d4fcc897e41f6
ctrmint <mark@mrodman.com>
2017-06-17 23:13:57 +0100

    Start

--- fileutil.py
+++ fileutil.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 # Handy file utils
 
-print "test"
+
+class myfile(object):
+    def __init__(self):
+        self.name = filename
+        self.size = 0
+        self.type = ""
+
+    def getsize(self):
+        print "get size"
+
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
